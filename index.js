b732036d53ff5a7a591a97a0e8f1be50a2c7e29d
marianovungel <vungem88@gmail.com>
2022-04-18 06:25:20 -0300

    stripe

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const app = express()
 //importações
 const mongoose = require('mongoose')
 const morgan = require('morgan')
+const cors = require('cors')
 const authRouter = require('./routes/auth.routes')
 const userRouter = require('./routes/user.routes')
 const productRouter = require('./routes/product.routes')
 const cartRouter = require('./routes/cart.routes')
 const orderRouter = require('./routes/order.routes')
-
-
-
+const stripeRouter = require('./routes/stripe.routes')
 
 
 //
 app.use(morgan())
+app.use(cors())
 app.use(express.json())
 mongoose.connect(process.env.URL_MONGDB, {
     useNewUrlParser: true,
@@ -28,12 +28,9 @@ mongoose.connect(process.env.URL_MONGDB, {
 app.use('/auth', authRouter)
 app.use('/product', productRouter)
 app.use('/user', userRouter)
+app.use('/stripe', stripeRouter)
 app.use('/order', orderRouter)
 app.use('/cart', cartRouter)
-
-
-
-
 
 port = process.env.PORT;
 app.listen(port, console.log('porta '+ port))
